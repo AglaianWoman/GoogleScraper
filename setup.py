060f6c58e22c526610d8b7683b13fb7aca802da7
Ronald Schmidt <ronald.schmidt@sparwelt.de>
2017-02-23 16:54:46 +0100

    update requirements lock versions, small style fixes

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import re
 from setuptools import setup
 
@@ -11,16 +10,17 @@ version = re.search(
 
 requirements = [r for r in open('requirements.txt', 'r').read().split('\n') if r]
 
-setup(name='GoogleScraper',
-      version=version,
-      description='A module to scrape and extract links, titles and descriptions from various search engines. Supports google,bing,yandex and many more.',
-      long_description=open('README.md').read(),
-      author='Nikolai Tschacher',
-      author_email='admin@incolumitas.com',
-      url='http://incolumitas.com',
-      py_modules=['usage'],
-      packages=['GoogleScraper'],
-      entry_points={'console_scripts': ['GoogleScraper = GoogleScraper.core:main']},
-      package_dir={'examples': 'examples'},
-      install_requires=requirements
+setup(
+    name='GoogleScraper',
+    version=version,
+    description='A module to scrape and extract links, titles and descriptions from various search engines. Supports google,bing,yandex and many more.',
+    long_description=open('README.md').read(),
+    author='Nikolai Tschacher',
+    author_email='admin@incolumitas.com',
+    url='http://incolumitas.com',
+    py_modules=['usage'],
+    packages=['GoogleScraper'],
+    entry_points={'console_scripts': ['GoogleScraper = GoogleScraper.core:main']},
+    package_dir={'examples': 'examples'},
+    install_requires=requirements
 )
